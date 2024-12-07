2bc422b0f6d1cdca947badd6e099ea4c66839397
Remzi Ahmedov <130896825+Remzi85@users.noreply.github.com>
2024-12-07 13:28:34 +0400

    Add files via upload

---
+++ lessontask_1/Program.cs
@@ -0,0 +1,87 @@
+﻿namespace lessontask_1
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("=== Create Your Weapon ===");
+            Console.Write("Enter comb capacity: ");
+            int capacity = int.Parse(Console.ReadLine());
+            Console.Write("Enter bullets in comb: ");
+            int bulletsInComb = int.Parse(Console.ReadLine());
+            Console.Write("Enter fire mode (single/triple/automatic): ");
+            string fireMode = Console.ReadLine();
+
+            Weapon weapon = new Weapon(capacity, bulletsInComb, fireMode);
+
+            while (true)
+            {
+                Console.WriteLine("\n=== Options ===");
+                Console.WriteLine("0 - Get Information");
+                Console.WriteLine("1 - Shoot");
+                Console.WriteLine("2 - Fire");
+                Console.WriteLine("3 - Get Remaining Bullet Count");
+                Console.WriteLine("4 - Reload");
+                Console.WriteLine("5 - Change Fire Mode");
+                Console.WriteLine("6 - Quit");
+                Console.WriteLine("7 - Edit Options");
+                Console.WriteLine("  8 - Change Comb Capacity");
+                Console.WriteLine("  9 - Change Number of Bullets in Comb");
+
+                Console.Write("Choose an option: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 0:
+                        weapon.GetInfo();
+                        break;
+                    case 1:
+                        weapon.Shoot();
+                        break;
+                    case 2:
+                        weapon.Fire();
+                        break;
+                    case 3:
+                        Console.WriteLine($"Bullets needed to fill the comb: {weapon.GetRemainBulletCount()}");
+                        break;
+                    case 4:
+                        weapon.Reload();
+                        break;
+                    case 5:
+                        weapon.ChangeFireMode();
+                        break;
+                    case 6:
+                        Console.WriteLine("Exiting the program. Goodbye!");
+                        return;
+                    case 7:
+                        Console.WriteLine("\n=== Edit Options ===");
+                        Console.WriteLine("8 - Change Comb Capacity");
+                        Console.WriteLine("9 - Change Number of Bullets in Comb");
+                        Console.Write("Choose an edit option: ");
+                        int subChoice = int.Parse(Console.ReadLine());
+                        if (subChoice == 8)
+                        {
+                            Console.Write("Enter new comb capacity: ");
+                            int newCapacity = int.Parse(Console.ReadLine());
+                            weapon.EditCapacity(newCapacity);
+                        }
+                        else if (subChoice == 9)
+                        {
+                            Console.Write("Enter new number of bullets in comb: ");
+                            int newBullets = int.Parse(Console.ReadLine());
+                            weapon.EditBullets(newBullets);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Invalid edit option.");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ lessontask_1/WeaponClass.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lessontask_1
+{
+    public class Weapon
+    {
+        private int capacity;
+        private int bulletsInComb;
+        private string fireMode;
+
+        public Weapon(int capacity, int bulletsInComb, string fireMode)
+        {
+            if (capacity <= 0 || bulletsInComb < 0 || bulletsInComb > capacity)
+                throw new ArgumentException("Invalid capacity or number of bullets.");
+            if (fireMode != "single" && fireMode != "triple" && fireMode != "automatic")
+                throw new ArgumentException("Invalid fire mode. Use 'single', 'triple', or 'automatic'.");
+
+            this.capacity = capacity;
+            this.bulletsInComb = bulletsInComb;
+            this.fireMode = fireMode;
+        }
+
+        public void Shoot()
+        {
+            if (bulletsInComb > 0)
+            {
+                bulletsInComb--;
+                Console.WriteLine("Fired 1 bullet.");
+            }
+            else
+            {
+                Console.WriteLine("No bullets left in the comb!");
+            }
+        }
+
+        public void Fire()
+        {
+            if (fireMode == "single")
+            {
+                Console.WriteLine("Firing all bullets one by one...");
+                while (bulletsInComb > 0) Shoot();
+            }
+            else if (fireMode == "triple")
+            {
+                Console.WriteLine("Firing in burst mode...");
+                for (int i = 0; i < 3 && bulletsInComb > 0; i++) Shoot();
+            }
+            else if (fireMode == "automatic")
+            {
+                Console.WriteLine("Firing all bullets in automatic mode...");
+                while (bulletsInComb > 0) Shoot();
+            }
+        }
+
+        public int GetRemainBulletCount()
+        {
+            return capacity - bulletsInComb;
+        }
+
+        public void Reload()
+        {
+            int bulletsNeeded = capacity - bulletsInComb;
+            bulletsInComb = capacity;
+            Console.WriteLine($"Reloaded {bulletsNeeded} bullets.");
+        }
+
+        public void ChangeFireMode()
+        {
+            if (fireMode == "single")
+                fireMode = "triple";
+            else if (fireMode == "triple")
+                fireMode = "automatic";
+            else if (fireMode == "automatic")
+                fireMode = "single";
+
+            Console.WriteLine($"Fire mode changed to {fireMode}.");
+        }
+
+        public void EditCapacity(int newCapacity)
+        {
+            if (newCapacity <= 0)
+                throw new ArgumentException("Capacity must be greater than 0.");
+            capacity = newCapacity;
+            if (bulletsInComb > capacity) bulletsInComb = capacity;
+            Console.WriteLine($"Comb capacity changed to {newCapacity}.");
+        }
+
+        public void EditBullets(int newBullets)
+        {
+            if (newBullets < 0 || newBullets > capacity)
+                throw new ArgumentException("Invalid bullet count.");
+            bulletsInComb = newBullets;
+            Console.WriteLine($"Bullets in comb set to {newBullets}.");
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Capacity: {capacity}, Bullets in Comb: {bulletsInComb}, Fire Mode: {fireMode}");
+        }
+    }
+
+}
